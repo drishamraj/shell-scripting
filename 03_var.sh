7f06834d211a4ce1c5f41c30396735ca990dac02
ashish <ashishkumar32750@gmail.com>
2024-05-19 00:00:03 +0000

    03 commit

---
+++ 03_var.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+name="ashish"
+age=21
+
+echo "My name is $name and age is $age"
+
+#var to store the output of the command
+
+HOSTNAME=$(hostname)
+
+echo "Name of the machine is $HOSTNAME"
+
